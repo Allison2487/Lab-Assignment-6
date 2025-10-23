119ee4d4f39b4f72e5f68c3aa42b7266336a82d1
Allison2487 <Allisonleung75@gmail.com>
2025-10-23 09:49:20 -0400

    Part 3, done by Allison and Ava

--- Part 3-4.py
+++ Part 3-4.py
@@ -2,6 +2,46 @@
 """
 Created on Thu Oct 23 08:14:49 2025
 
-@author: 2487300
+@author: Allison Leung, Ava Binetti
 """
+import pandas as pd
+import seaborn as sns
 
+data= pd.read_csv("data.csv")
+
+#Part 3
+
+#Q3
+data_1= data[["Physicians", "Population"]]
+pd.DataFrame.info(data_1)
+#there are 10 empty cells in "physicians" and none in "population"
+
+#Q4
+print(pd.DataFrame.nunique(data))
+
+#Q5
+print(pd.DataFrame.describe(data))
+#It provides the count of values, the mean, standard deviation, min and max value and level of confidence(25%,50%,75%) for columns with numerical values.
+
+#Q6
+data["GNI per Capita"] = data["GNI"]/data["Population"]
+data["GNI per Capita"]=round(data["GNI per Capita"],2) # Rounds column to 2 decimals.
+print(data["GNI per Capita"])
+
+#Q7
+Region_count= data["Region"].value_counts() #Counts how many of each variable in a column
+print(Region_count)
+# There are 56 countries in Africa, 50 in Asia, 47 in Europe, 46 in Americas and 19 in Oceania
+
+HIE_count = data["High Income Economy"].value_counts()
+print(HIE_count)
+# There are 67 countries with HIE and 150 that don't
+
+#Q8
+HIE_countries=pd.crosstab(data["Region"], data["High Income Economy"]) #Counts how many 1 and 0 (Yes and no) in HIE column sorted by Region
+print(HIE_countries)
+#There are 17 HIE countries in Amercias, 14 in Asia, 31 in Europe and 4 in Oceania.
+
+#Q9
+if data["Life expectancy, female"] > 80:
+    print()
